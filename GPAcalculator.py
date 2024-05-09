9de5f3cdd3e2f431dcbe92adc6423bcf37cfc6c3
cbijeaux <115676825+cbijeaux@users.noreply.github.com>
2024-05-09 11:40:46 -0400

    Update GPAcalculator.py
    don't remember

--- GPAcalculator.py
+++ GPAcalculator.py
@@ -56,6 +56,26 @@ def addCriteria(gpaclass):
     section=input(f'Please enter the name of the section:\n')
     percentage=input(f'Please enter the percentage of the section on the final grade (example: %15 or 15):\n')
     gpaclass.addCriteria(section,percentage/100)
+def removeCriteria(gpaclass):
+    counter=0
+    for element in gpaclass.pullSections():
+        counter+=1
+        print(f'{counter}) {element}')
+    choice=int(input(f'Please choose a section you want to remove (this will also remove all the grades from that section)\n'))-1
+    gpaclass.removeCriteria(gpaclass.pullSections()[choice])
+def removeGrade(gpaclass):
+    counter=0
+    for element in gpaclass.pullSections():
+        counter+=1
+        print(f'{counter}) {element}')
+    choice=int(input(f'Please choose a section you would like to remove a grade from'))
+    grade=gpaclass.pullGrades(gpaclass.pullSections()[choice])
+    if len(grade)==1:
+        gpaclass.removeGrade(title)
+    else:
+        print(grade)
+        choice=int(input(f'Please enter a grade you would like to remove, or -1 if you wish to remove all the grades from the section\n'))
+        gpaclassremoveGrade(title,choice)
 def mainMenu(allfiles,directorypathing):
     global gpa
     if len(allfiles)==0:
@@ -75,6 +95,7 @@ def mainMenu(allfiles,directorypathing):
     elif int(choice)==1:
         title=input(f'Please enter the identifier you want for this course\n').strip()+f'.txt'
         gpa=WeightedGPA()
+    
                                 
 notfinished=True
 directorypathing=pathing_creator(f'stored')
@@ -82,26 +103,32 @@ allfiles=directory.pullAllDirectoryFiles(directorypathing)
 print(f'Welcome to the GPA calculator. This is to assist in calculating GPAs in specific classes that use the weighted GPA system (with percentages)]\n')
 mainMenu(allfiles,directorypathing)
 while notfinished:
-    choice=input(f'Please choose an option below:\n1)Add Criteria or grade\n2)See Current Final Grade\n3)Check what I need on final assignment/exam for a certain grade\n4)see all grades\n5)Save and Return to Main Menu\n6)Save and Exit Program\n')
+    choice=input(f'Please choose an option below:\n1)Add Criteria or grade\n2)Remove criteria or grade\n3)See Current Final Grade\n4)Check what I need on final assignment/exam for a certain grade\n5)see all grades\n6)Save and Return to Main Menu\n7)Save and Exit Program\n')
     realchoice=int(choice)
     if realchoice==1:
-        choice=input(f'please choose an options below: 1)Add Criteria\n2) Add Grade\n')
+        choice=input(f'please choose an options below:\n1)Add Criteria\n2) Add Grade\n')
         if choice=='1':
             addCriteria(gpa)
         else:
             addGrades(gpa)
     elif realchoice==2:
-        print(f'Your final grade in this class so far is {gpa.getFinalGrade()}\nNOTE: accuracy of the final grade is dependent on whether all the grades have been entered or not\n')
+        choice=int(input(f'Please choose an options below:\n1) Remove Criteria\n2) Remove Grade'))
+        if choice==1:
+            gpa.removeCriteria(gpa)
+        elif choice==2:
+            gpa.removeGrade(gpa)
     elif realchoice==3:
+        print(f'Your final grade in this class so far is {gpa.getFinalGrade()}\nNOTE: accuracy of the final grade is dependent on whether all the grades have been entered or not\n')
+    elif realchoice==4:
         final=input(f'What section does the final assignment/exam belong to?\n')
         gpa.whatDoINeed(final)
-    elif realchoice==4:
-        print(gpa)
     elif realchoice==5:
+        print(gpa)
+    elif realchoice==6:
         saveClass(directorypathing,title,gpa.convertData())
         print(f'Save Completed! Returning to Main Menu)'
         mainMenu(allfiles,directorypathing)
-    elif realchoice==6:
+    elif realchoice==7:
         saveClass(directorypathing,title,gpa.convertData())
         notfinished=False
 print(f'Save Completed! GoodBye')
